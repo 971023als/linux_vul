2358e30eab4cceebb51c686ff386689f87b9b280
971023als <971023als@naver.com>
2024-04-11 01:33:43 +0900

    28

--- Python_json/ubuntu/U-28.py
+++ Python_json/ubuntu/U-28.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python3
 import subprocess
-import os
-import re
-import json
-
-def check_nis_services_status():
-    results = {
-        "분류": "서비스 관리",
-        "코드": "U-28",
-        "위험도": "상",
-        "진단 항목": "NIS, NIS+ 점검",
-        "진단 결과": None,  # 초기 상태 설정, 검사 후 결과에 따라 업데이트
-        "현황": [],
-        "대응방안": "NIS 서비스 비활성화 혹은 필요 시 NIS+ 사용"
-    }
 
 def check_nis_processes():
     nis_processes = ['ypserv', 'ypbind', 'ypxfrd', 'rpc.yppasswdd', 'rpc.ypupdated']
     try:
-        # ps 명령을 사용하여 실행 중인 프로세스를 확인
+        # 실행 중인 모든 프로세스를 확인
         result = subprocess.Popen(["ps", "-e"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         output, _ = result.communicate()
-        output_lines = output.split('\n')
-        for line in output_lines:
-            if any(nis_process in line.lower() for nis_process in nis_processes):
+        for process in nis_processes:
+            if process in output.lower():
                 return True
         return False
     except Exception as e:
@@ -31,7 +16,16 @@ def check_nis_processes():
         return False
 
 def main():
-    results = {"진단 결과": "", "현황": []}
+    results = {
+        "분류": "서비스 관리",
+        "코드": "U-28",
+        "위험도": "상",
+        "진단 항목": "NIS, NIS+ 점검",
+        "진단 결과": None,
+        "현황": [],
+        "대응방안": "NIS 서비스 비활성화 혹은 필요 시 NIS+ 사용"
+    }
+
     try:
         if check_nis_processes():
             results["진단 결과"] = "취약"
@@ -43,7 +37,7 @@ def main():
         results["진단 결과"] = "오류"
         results["현황"].append(f"NIS 서비스 확인 중 오류 발생: {str(e)}")
 
-    return results
+    print(results)
 
 if __name__ == "__main__":
-    print(main())
+    main()
