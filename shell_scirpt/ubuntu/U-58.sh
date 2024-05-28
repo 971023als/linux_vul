a28d54cbdaa4c3cad46ff123c123160c6755fa60
971023als <971023als@naver.com>
2024-05-28 19:50:38 +0900

    54~60

--- shell_scirpt/ubuntu/U-58.sh
+++ shell_scirpt/ubuntu/U-58.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# 변수 초기화
-results=""
-vulnerability_found=false
+OUTPUT_CSV="output.csv"
+
+# Set CSV Headers if the file does not exist
+if [ ! -f $OUTPUT_CSV ]; then
+    echo "category,code,riskLevel,diagnosisItem,service,diagnosisResult,status" > $OUTPUT_CSV
+fi
+
+# Initial Values
 category="파일 및 디렉토리 관리"
 code="U-58"
-severity="중"
-check_item="홈디렉토리로 지정한 디렉토리의 존재 관리"
-status=()
-recommendation="홈 디렉터리가 존재하지 않는 계정이 없도록 관리"
+riskLevel="중"
+diagnosisItem="홈디렉토리로 지정한 디렉토리의 존재 관리"
+service="File and Directory Management"
+diagnosisResult=""
+status=""
 
 # 모든 사용자 계정을 확인
 while IFS=: read -r username _ uid _ _ home_dir shell; do
@@ -16,32 +22,24 @@ while IFS=: read -r username _ uid _ _ home_dir shell; do
   if [ "$uid" -ge 1000 ] && [[ "$shell" != *"nologin" ]] && [[ "$shell" != *"false" ]]; then
     # 홈 디렉터리가 존재하지 않거나, 관리자가 아닌 계정의 홈 디렉터리가 '/' 인 경우
     if [ ! -d "$home_dir" ] || { [ "$home_dir" == "/" ] && [ "$username" != "root" ]; }; then
-      vulnerability_found=true
       if [ ! -d "$home_dir" ]; then
-        status+=("$username 계정의 홈 디렉터리 ($home_dir) 가 존재하지 않습니다.")
+        diagnosisResult="$username 계정의 홈 디렉터리 ($home_dir) 가 존재하지 않습니다."
+        status="취약"
       elif [ "$home_dir" == "/" ]; then
-        status+=("관리자 계정(root)이 아닌데 $username 계정의 홈 디렉터리가 '/'로 설정되어 있습니다.")
+        diagnosisResult="관리자 계정(root)이 아닌데 $username 계정의 홈 디렉터리가 '/'로 설정되어 있습니다."
+        status="취약"
       fi
+      echo "$category,$code,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
     fi
   fi
 done < /etc/passwd
 
 # 진단 결과 설정
-if $vulnerability_found; then
-  result="취약"
-else
-  result="양호"
-  status=("모든 사용자 계정의 홈 디렉터리가 적절히 설정되어 있습니다.")
+if ! grep -q "취약" $OUTPUT_CSV; then
+  diagnosisResult="모든 사용자 계정의 홈 디렉터리가 적절히 설정되어 있습니다."
+  status="양호"
+  echo "$category,$code,$riskLevel,$diagnosisItem,$service,$diagnosisResult,$status" >> $OUTPUT_CSV
 fi
 
-# 결과 출력
-echo "분류: $category"
-echo "코드: $code"
-echo "위험도: $severity"
-echo "진단 항목: $check_item"
-echo "진단 결과: $result"
-echo "현황:"
-for i in "${status[@]}"; do
-  echo "- $i"
-done
-echo "대응방안: $recommendation"
+# Output CSV
+cat $OUTPUT_CSV
