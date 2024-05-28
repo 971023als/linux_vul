019db13bfe85b5b91bf564b4a136a0ace0fb88fa
971023als <971023als@naver.com>
2024-05-28 19:27:07 +0900

    17 18 19

--- shell_scirpt/ubuntu/U-18.sh
+++ shell_scirpt/ubuntu/U-18.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+OUTPUT_JSON="output.json"
+
 # 변수 설정
 분류="파일 및 디렉터리 관리"
 코드="U-18"
 위험도="상"
-진단_항목="접속 IP 및 포트 제한"
+진단항목="접속 IP 및 포트 제한"
 대응방안="특정 호스트에 대한 IP 주소 및 포트 제한 설정"
 현황=()
 
 hosts_deny_path='/etc/hosts.deny'
 hosts_allow_path='/etc/hosts.allow'
+진단결과=""
 
 # 파일 존재 및 내용 검사 함수
 check_file_exists_and_content() {
@@ -25,26 +28,29 @@ check_file_exists_and_content() {
 
 # /etc/hosts.deny 검사
 if ! check_file_exists_and_content "$hosts_deny_path" "ALL: ALL"; then
-    진단_결과="취약"
+    진단결과="취약"
     현황+=("$hosts_deny_path 파일에 'ALL: ALL' 설정이 없거나 파일이 없습니다.")
 else
     # /etc/hosts.allow 검사
     if check_file_exists_and_content "$hosts_allow_path" "ALL: ALL"; then
-        진단_결과="취약"
+        진단결과="취약"
         현황+=("$hosts_allow_path 파일에 'ALL: ALL' 설정이 있습니다.")
     else
-        진단_결과="양호"
+        진단결과="양호"
         현황+=("적절한 IP 및 포트 제한 설정이 확인되었습니다.")
     fi
 fi
 
-# 결과 출력
-echo "분류: $분류"
-echo "코드: $코드"
-echo "위험도: $위험도"
-echo "진단 항목: $진단_항목"
-echo "대응방안: $대응방안"
-echo "진단 결과: $진단_결과"
-for item in "${현황[@]}"; do
-    echo "$item"
-done
+# 결과를 JSON 파일에 기록
+echo "{
+    \"분류\": \"$분류\",
+    \"코드\": \"$코드\",
+    \"위험도\": \"$위험도\",
+    \"진단항목\": \"$진단항목\",
+    \"대응방안\": \"$대응방안\",
+    \"진단결과\": \"$진단결과\",
+    \"현황\": [$(printf '%s\n' "${현황[@]}" | jq -R . | jq -s .)]
+}" > $OUTPUT_JSON
+
+# JSON 파일 출력
+cat $OUTPUT_JSON
--- shell_scirpt/ubuntu/U-19.sh
+++ shell_scirpt/ubuntu/U-19.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+OUTPUT_JSON="output.json"
+
 # 변수 설정
 분류="서비스 관리"
 코드="U-19"
 위험도="상"
-진단_항목="Finger 서비스 비활성화"
+진단항목="Finger 서비스 비활성화"
 대응방안="Finger 서비스가 비활성화 되어 있는 경우"
 현황=()
-진단_결과=""
+진단결과=""
 
 # /etc/services에서 Finger 서비스 정의 확인
 if grep -iq "^finger.*tcp" /etc/services; then
     현황+=("Finger 서비스 포트가 /etc/services에 정의되어 있습니다.")
-    진단_결과="취약"
+    진단결과="취약"
 else
     if [ ! -f "/etc/services" ]; then
         현황+=("/etc/services 파일을 찾을 수 없습니다.")
@@ -22,21 +24,24 @@ fi
 # Finger 프로세스 실행 중인지 확인
 if ps -ef | grep -iq "finger"; then
     현황+=("Finger 서비스 프로세스가 실행 중입니다.")
-    진단_결과="취약"
+    진단결과="취약"
 fi
 
-if [ -z "$진단_결과" ]; then
-    진단_결과="양호"
+if [ -z "$진단결과" ]; then
+    진단결과="양호"
     현황+=("Finger 서비스가 비활성화되어 있거나 실행 중이지 않습니다.")
 fi
 
-# 결과 출력
-echo "분류: $분류"
-echo "코드: $코드"
-echo "위험도: $위험도"
-echo "진단 항목: $진단_항목"
-echo "대응방안: $대응방안"
-echo "진단 결과: $진단_결과"
-for item in "${현황[@]}"; do
-    echo "$item"
-done
+# 결과를 JSON 파일에 기록
+echo "{
+    \"분류\": \"$분류\",
+    \"코드\": \"$코드\",
+    \"위험도\": \"$위험도\",
+    \"진단항목\": \"$진단항목\",
+    \"대응방안\": \"$대응방안\",
+    \"진단결과\": \"$진단결과\",
+    \"현황\": [$(printf '%s\n' "${현황[@]}" | jq -R . | jq -s .)]
+}" > $OUTPUT_JSON
+
+# JSON 파일 출력
+cat $OUTPUT_JSON
